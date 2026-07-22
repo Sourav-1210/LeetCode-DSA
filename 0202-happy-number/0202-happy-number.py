24eb76511414375d0b1f369bf228948ac0792b28
Sourav Sharma <153025557+Sourav-1210@users.noreply.github.com>
2026-07-22 14:47:45 +0530

    Time: 1 ms (47.83%), Space: 19.2 MB (61.41%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isHappy(self, n):
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(c) ** 2 for c in str(n))
+
+        return n == 1
