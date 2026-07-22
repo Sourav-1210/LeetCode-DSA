001ac2d7dace9211e2fc5c4324c7a5f7d44f24ba
Sourav Sharma <153025557+Sourav-1210@users.noreply.github.com>
2026-07-22 14:51:58 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.98%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordPattern(self, pattern, s):
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        p_map = {}
+        w_map = {}
+
+        for p, w in zip(pattern, words):
+            if (p in p_map and p_map[p] != w) or (w in w_map and w_map[w] != p):
+                return False
+
+            p_map[p] = w
+            w_map[w] = p
+
+        return True
