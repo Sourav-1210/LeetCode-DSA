7ffbd2053db80c51a229afd40768e82fccf59d60
Sourav Sharma <153025557+Sourav-1210@users.noreply.github.com>
2026-08-01 15:02:51 +0530

    Time: 4 ms (46.4%), Space: 44.8 MB (42.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
+        for(char ch:s.toCharArray()){
             if(Character.isLetterOrDigit(ch)){
                 sb.append(Character.toLowerCase(ch));
             }
         }
         int i=0;
-        int j=sb.length()-1;
+        int j = sb.length()-1;
         while(i<j){
             if(sb.charAt(i)!=sb.charAt(j)){
                 return false;
