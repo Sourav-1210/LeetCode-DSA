b4c0d452d0c2fd8780a32d2cf326bce553b6097a
Sourav Sharma <153025557+Sourav-1210@users.noreply.github.com>
2026-09-08 19:22:02 +0530

    Time: 302 ms (27.05%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,16 +1,7 @@
-# Write your MySQL query statement below
-SELECT machine_id,
-       ROUND(AVG(end_time - start_time), 3) AS processing_time
-FROM (
-    SELECT machine_id,
-           process_id,
-           MAX(CASE
-                   WHEN activity_type = 'start' THEN timestamp
-               END) AS start_time,
-           MAX(CASE
-                   WHEN activity_type = 'end' THEN timestamp
-               END) AS end_time
-    FROM Activity
-    GROUP BY machine_id, process_id
-) AS t
-GROUP BY machine_id;
+# Write your MySQL query stAVG(value)
+SELECT a1.machine_id,ROUND(AVG(a2.timestamp - a1.timestamp),3)
+AS processing_time FROM
+Activity a1 JOIN Activity a2
+ON a1.machine_id = a2.machine_id
+WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
